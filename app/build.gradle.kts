142f6025a27ec0424d21a99fa00bc3eda187f3b1
bercze.zsombor <berczezsombor1996@gmail.com>
2023-10-14 19:36:35 +0200

    changed to compile sdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "hu.nje.foodtinder"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "hu.nje.foodtinder"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
