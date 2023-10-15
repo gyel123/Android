c4f5598845a6f1c53ac4393739508cd009960227
Jenesxd <jenesnorbert@gmail.com>
2023-10-15 02:14:45 +0200

    Fragmentek közti lépkedés, debug

--- app/src/main/java/hu/nje/foodtinder/view/MainActivity.java
+++ app/src/main/java/hu/nje/foodtinder/view/MainActivity.java
@@ -1,34 +1,60 @@
 package hu.nje.foodtinder.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.View.OnClickListener;
+
+
+import hu.nje.foodtinder.R;
+import hu.nje.foodtinder.view.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void loadFragment(Fragment fragment, String tag) {
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment, tag);
+        fragmentTransaction.commit();
+
+
+    }
+
+
+
+    private void loadFragmentAndAddToBackStack(Fragment fragment, String tag) {
+        loadFragment(fragment, tag, true);
+    }
+
+    private void loadFragment(Fragment fragment, String tag, boolean addToBackStack) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment, tag);
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(tag);
+        }
+        fragmentTransaction.commit();
+    }
+
+
+    private Button buttonNavigate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Gomb referenciájának megszerzése az azonosító alapján
-        Button buttonNavigate = findViewById(R.id.buttonSaveRecipes);
+        buttonNavigate = findViewById(R.id.buttonSaveRecipes);
 
-        // Gomb kattintáskezelőjének beállítása
         buttonNavigate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // Átnavigálás a fragmentre
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, new irfragment(v)) // MyFragment helyettesítése a megfelelő fragmenttel
-                        .addToBackStack(null)
-                        .commit();
+            public void onClick(View view) {
+                // Hozz létre egy új Fragment példányt
+                loadFragment(masodikablak);
+
             }
         });
     }
 }
-
-
-
